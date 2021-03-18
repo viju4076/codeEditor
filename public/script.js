98605090f3d35a04384bb0efe10a2606fafb6e15
xhreya <shreyasharma943085@gmail.com>
2021-03-18 20:56:41 +0530

    chat box added

--- public/script.js
+++ public/script.js
@@ -175,10 +175,45 @@ socket.on('codeResult',response=>{
 
 })
 
+
 function transmitCode(){
     console.log("called")
     socket.emit('editor-change',{text : editor.getValue()})
 }
 
 {}
+//chat section 
+
+var message = document.getElementById('message'),
+handle = document.getElementById('handle'),
+btn = document.getElementById('send'),
+output = document.getElementById('output'),
+feedback=document.getElementById('feedback');
+
+
+btn.addEventListener('click',function(){
+socket.emit('chat', {
+  message: message.value,
+  handle: handle.value
+});
+
+message.value="";
+});
+message.addEventListener('keypress',function(){
+socket.emit('typing',handle.value);
+});
+
+socket.on('chat', function(data){
+feedback.innerHTML="";
+output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+
+}); 
+
+socket.on('typing', function(data){
+feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>';
+});
+//chat end
+
+
+
   
