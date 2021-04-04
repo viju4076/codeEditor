d1b5761ac0297db8839008042383213fb23f0593
viju4076 <vijaykrishna0205@gmail.com>
2021-04-04 10:03:06 +0530

    small change in index.js

--- index.js
+++ index.js
@@ -155,13 +155,13 @@ function (error, response, body) {
 })
 
 
-let port=process.env.PORT;
-if(port==null||port=="")
-{
-  port=8000;
-}
+// let port=process.env.PORT;
+// if(port==null||port=="")
+// {
+//   port=8000;
+// }
 
-server.listen(port) 
+server.listen(8080); 
 
 
 
